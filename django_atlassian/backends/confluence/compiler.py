a05b2ee74df8b2daa364e3c6aa915a68368f5d86
Jorge Zapata <jorgeluis.zapata@gmail.com>
2022-07-24 22:36:04 +0200

    Remove the ORM bindings from the project

--- django_atlassian/backends/confluence/compiler.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django_atlassian.backends.common import compiler
-
-class SQLCompiler(compiler.SQLCompiler):
-    query_param = 'cql'
