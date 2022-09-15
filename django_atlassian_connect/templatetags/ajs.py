4ea64d83b4b0c93b98eeab62505a1d122eaa4b58
Jorge Zapata <jorgeluis.zapata@gmail.com>
2022-09-15 18:51:31 +0200

    Use the correct types

--- django_atlassian_connect/templatetags/ajs.py
+++ django_atlassian_connect/templatetags/ajs.py
@@ -6,8 +6,19 @@ register = template.Library()
 
 @register.simple_tag
 def ajs(resize=True, size_to_parent=False, margin=True, base=False):
+    def _to_js(val):
+        if val:
+            return "true"
+        else:
+            return "false"
+
+    resize_str = _to_js(resize)
+    size_to_parent_str = _to_js(size_to_parent)
+    margin_str = _to_js(margin)
+    base_str = _to_js(base)
+
     data_options = "resize:{};sizeToParent:{};margin:{};base:{}".format(
-        resize, size_to_parent, margin, base
+        resize_str, size_to_parent_str, margin_str, base_str
     )
     script_str = "<script src='https://connect-cdn.atl-paas.net/all.js' data-options='{}'></script>".format(
         data_options
