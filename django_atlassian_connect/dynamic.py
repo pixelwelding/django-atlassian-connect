fd1300d85d74c85460bed84645e270ab9eabd442
Jorge Zapata <jorgeluis.zapata@gmail.com>
2023-10-22 22:05:01 +0200

    Add a dynamic registry

---
+++ django_atlassian_connect/dynamic.py
@@ -0,0 +1,26 @@
+from jira.client import JIRA
+
+from django_atlassian_connect.properties import registry_properties
+
+
+class DynamicModuleRegistry:
+    def modules(self, sc):
+        ret = {}
+        if len(registry_properties):
+            ret["jiraEntityProperties"] = [
+                x.to_module(sc) for x in registry_properties if x.dynamic == True
+            ]
+        return ret
+
+    def register(self, sc):
+        j = JIRA(sc.host, jwt={"secret": sc.shared_secret, "payload": {"iss": sc.key}})
+        modules = self.modules(sc)
+        if modules:
+            j.register_dynamic_modules(modules)
+
+    def remove(self, sc):
+        j = JIRA(sc.host, jwt={"secret": sc.shared_secret, "payload": {"iss": sc.key}})
+        j.remove_dynamic_modules(modules)
+
+
+registry_dynamic_modules = DynamicModuleRegistry()
---
+++ django_atlassian_connect/tests/test_dynamic.py
@@ -0,0 +1,6 @@
+def fake_register(self, sc):
+    pass
+
+
+def fake_remove(self, sc):
+    pass
